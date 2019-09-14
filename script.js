108ea0fc550238b3b525d1ad81644a9f92117006
Damian Ciasnocha <daneek80@gmail.com>
2019-09-14 23:21:25 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+// Array
+
+const quotes = [
+    {
+     name:'Walt Disney'
+     quotes: 'All our dreams can come true - if we have the courage to pursue them.'
+    },
+    {
+    name: 'Mother Teresa'
+    quotes: 'I have found the paradox that if I love until it hurts, then there is no hurt, but only more love.  '
+    },
+    {
+        name: 'Mat Ghandi'
+     quotes: 'Where there is love there is life.'
+    },
+    {
+    name: 'Walt Disney'
+     quotes: 'All our dreams can come true - if we have the courage to pursue them.'
+    },
+    {
+    name: 'Walt Disney'
+     quotes: 'All our dreams can come true - if we have the courage to pursue them.'
+    },
+
+]
+// 1.targeting button tag
