7fcc3bea631b4b54f76a97cd3d5b3d30b7b0cd0f
Damian Ciasnocha <daneek80@gmail.com>
2019-09-15 07:15:30 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,28 +1,4 @@
-// Array
 
-const quotes = [
-    {
-     name:'Walt Disney'
-     quotes: 'All our dreams can come true - if we have the courage to pursue them.'
-    },
-    {
-    name: 'Mother Teresa'
-    quotes: 'I have found the paradox that if I love until it hurts, then there is no hurt, but only more love.  '
-    },
-    {
-        name: 'Mat Ghandi'
-     quotes: 'Where there is love there is life.'
-    },
-    {
-    name: 'Walt Disney'
-     quotes: 'All our dreams can come true - if we have the courage to pursue them.'
-    },
-    {
-    name: 'Walt Disney'
-     quotes: 'All our dreams can come true - if we have the courage to pursue them.'
-    },
-
-]
 // 1.targeting button tag
 const quoteBtn = document.querySelector('#quoteBtn');
 // 2.targeting quote author section
@@ -39,7 +15,40 @@ quoteBtn.addEventListener('click', displayQuote);
 //  and create METHOD 'Math.random'
 //  which allows to generates random number(of quotes) for 'displayQuote'
 //  which is not less than 0 and bigger than 5 in our situation
+//  so to make sure the number will never exceed the last quote or go lower then 0 we are using'.length'
 
 function displayQuote()  {
-  let number = Math.floor(Math.random()*quotes.length)
+let number = Math.floor(Math.random()*quotes.length);
+// 3.let select name and quote from our 'Array'
+// we going to use 'innerHTML' property
+quoteAuthor.innerHTML = quotes[number].name;
+quote.innerHTML = quotes[number].quote;
 }
+
+
+
+// Array
+
+const quotes = [
+    {
+    name: 'Walt Disney',
+    quote: 'All our dreams can come true - if we have the courage to pursue them.'
+    },
+    {
+        name: 'Mother Teresa',
+    quote: 'I have found the paradox that if I love until it hurts, then there is no hurt, but only more love.  '
+    },
+    {
+        name: 'Mat Ghandi',
+     quote: 'Where there is love there is life.'
+    },
+    {
+        name: 'Damian Ciasnocha',
+     quote: 'Oh Joy.'
+    },
+    {
+        name: 'Julia Ciasnocha',
+     quote: 'Yyyyyyyyyyyyyyyyy.'
+    },
+
+]
